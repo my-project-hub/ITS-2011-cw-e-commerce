52f1cb7ba27212b28311a5fdb7daa6a19afc8c51
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2024-05-23 22:14:28 +0530

    create text editor

--- FRONTEND/src/component/text-editor.component.tsx
+++ FRONTEND/src/component/text-editor.component.tsx
@@ -1,8 +1,16 @@
-export default function TextEditor(){
+import { useState } from "react"
+import ReactQuill from "react-quill"
+import 'react-quill/dist/quill.snow.css';
+
+
+export default function TextEditor() {
+
+    const [value,setValue] = useState("")
+
     return (
         <>
-            <div>
-                
+            <div className="h-[150px]">
+                <ReactQuill className="h-full block relative" theme="snow" value={value} onChange={setValue} />
             </div>
         </>
     )
