19a01b04689310ec9b9d59c98a5d84447cac5dc0
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2024-05-23 12:57:07 +0530

    update multilevel slider component

--- FRONTEND/src/component/multilevel-sidebar.component.tsx
+++ FRONTEND/src/component/multilevel-sidebar.component.tsx
@@ -72,7 +72,7 @@ export default function MultiLevelSidebar() {
             strokeWidth={2.5}
             className={`mx-auto h-4 w-4 transition-transform ${open === 2 ? "rotate-180" : ""}`} />}  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}        >
           <ListItem className="p-0" selected={open === 2}  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-            <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-3">
+            <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-3" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
               <ListItemPrefix>
                 <ShoppingBagIcon className="h-5 w-5" />
               </ListItemPrefix>
