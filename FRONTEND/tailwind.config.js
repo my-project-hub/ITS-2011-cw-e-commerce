12333b51468dce9dfb9ac0f1795e8b8dbe451248
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2024-06-07 11:12:30 +0530

    update project

--- FRONTEND/tailwind.config.js
+++ FRONTEND/tailwind.config.js
@@ -1,16 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 export default withMT( {
+  darkMode:'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    require('flowbite/plugin'),
+    'node_modules/flowbite-react/lib/esm/**/*.{js,ts,jsx,tsx}',
+    // flowbite.content(),
+
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+      }
+    },
+    
   },
-  plugins: [],
+  plugins: [
+    // flowbite.plugin(),
+
+  ],
 }
 )
