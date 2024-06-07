540b7e97a54d46bff4362a56da917e67081b92f2
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2024-06-07 11:22:02 +0530

    update project

---
+++ FRONTEND/src/page/cusotmer/womens-wear/night-wear.page.tsx
@@ -0,0 +1,11 @@
+import CusotmerLayout from "../../../layout/cusotmer.layout";
+
+export function WomensNightWear(){
+    return(
+        <>
+            <CusotmerLayout>
+                <p>NightWear wear</p>
+            </CusotmerLayout>
+        </>
+    )
+}
