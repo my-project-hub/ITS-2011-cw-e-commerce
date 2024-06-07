2aefc630e7a3d1d8194c9ec3b0017d91a79c5a23
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2024-06-07 11:13:33 +0530

    update project

---
+++ FRONTEND/src/component/category-header-banner.components.tsx
@@ -0,0 +1,53 @@
+export function CategoryHeaderBanner() {
+    return (
+        <>
+            <div className="flex bg-[#dcd6d04d] items-center justify-center">
+                <div>
+                    <p className="font-semibold mb-3">Home | Category</p>
+                    <p className="text-6xl font-bold ">Women</p>
+                    <p className="font-semibold mb-6">Remos Women collection for you</p>
+                    <p className="font-semibold text-wrap">check the women collection in our store.Shop with us for a premium clothing,accesseried<br /> and fashion experience.</p>
+                    <div className="mt-5 flex gap-4">
+                        <div className="flex-row justify-center items-center">
+                            <div className="w-[70px] h-[70px] bg-red-500 rounded-full flex justify-center items-center">
+                                <img className="w-full h-full rounded-full" src="https://greencoding.b-cdn.net/nolimit.lk/cat_sq/Women,_Casual_Wear-desktop-1686894646196.jpeg" alt="" />
+                            </div>
+                            <small>Casual Wear</small>
+                        </div>
+
+                        <div className="flex-row justify-center items-center">
+                            <div className="w-[70px] h-[70px] bg-red-500 rounded-full flex justify-center items-center">
+                                <img className="w-full h-full rounded-full" src="https://greencoding.b-cdn.net/nolimit.lk/cat_sq/Women,_Casual_Wear-desktop-1686894646196.jpeg" alt="" />
+                            </div>
+                            <small>Casual Wear</small>
+                        </div>
+
+                        <div className="flex-row justify-center items-center">
+                            <div className="w-[70px] h-[70px] bg-red-500 rounded-full flex justify-center items-center">
+                                <img className="w-full h-full rounded-full" src="https://greencoding.b-cdn.net/nolimit.lk/cat_sq/Women,_Casual_Wear-desktop-1686894646196.jpeg" alt="" />
+                            </div>
+                            <small>Casual Wear</small>
+                        </div>
+
+                        <div className="flex-row justify-center items-center">
+                            <div className="w-[70px] h-[70px] bg-red-500 rounded-full flex justify-center items-center">
+                                <img className="w-full h-full rounded-full" src="https://greencoding.b-cdn.net/nolimit.lk/cat_sq/Women,_Casual_Wear-desktop-1686894646196.jpeg" alt="" />
+                            </div>
+                            <small>Casual Wear</small>
+                        </div>
+                        <div className="flex-row justify-center items-center">
+                            <div className="w-[70px] h-[70px] bg-red-500 rounded-full flex justify-center items-center">
+                                <img className="w-full h-full rounded-full" src="https://greencoding.b-cdn.net/nolimit.lk/cat_sq/Women,_Casual_Wear-desktop-1686894646196.jpeg" alt="" />
+                            </div>
+                            <small>Casual Wear</small>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className="p-28">
+                    <img className="rounded-lg" src="https://cdn.greencloudpos.com/nolimit.lk/cat_lo/Women-mobile-1698828760063.jpeg" alt="" />
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ FRONTEND/src/page/cusotmer/womens-wear/accessories.page.tsx
@@ -0,0 +1,11 @@
+import CusotmerLayout from "../../../layout/cusotmer.layout";
+
+export default function WomenAccessories(){
+    return (
+        <>
+            <CusotmerLayout>
+                <p>Women accessories</p>
+            </CusotmerLayout>
+        </>
+    );
+}
