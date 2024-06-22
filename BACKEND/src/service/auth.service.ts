46decb9bc79807ded0d0407674d1f1271decae66
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2024-06-22 15:00:57 +0530

    update project

--- BACKEND/src/service/auth.service.ts
+++ BACKEND/src/service/auth.service.ts
@@ -2,15 +2,24 @@ import { Request, Response } from "express";
 import usermodel from "../model/user.model";
 import bcrypt from 'bcryptjs'
 
-export async function validateUser(req:Request,res:Response){
-    const {email,password} = req.body;
-    //check the user available with the requested email
-    const user = await usermodel.findOne({email});
-    if(!user) res.json({message:"can not found user with email"}).status(401).send()
+export async function validateUser(req: Request, res: Response) {
+    try {
 
-    //if user is avaialble check the password
-    const validate = bcrypt.compareSync(user.password , password)
-    if(!validate) res.json({message:"password is wrong"}).status(401).send()
+        const { email, password } = req.body;
+        //check the user available with the requested email
+        const user = await usermodel.findOne({ email });
+        if (!user) throw new Error("can not found user with email");
 
-    res.end();
+        //if user is avaialble check the password
+        const validate = bcrypt.compareSync(password, user.password)
+        if (!validate) throw new Error("password is wrong")
+
+        res.end();
+
+    } catch (e) {
+        if(e.message === "can not found user with email") res.status(401).json({ message: e.message}).send()
+        if(e.message === "password is wrong") res.status(401).json({ message: e.message}).send()
+        res.status(500).send()
+    }
+
 }
