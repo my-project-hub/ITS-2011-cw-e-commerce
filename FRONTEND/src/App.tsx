555737dea81b5fbdbd57953c43e29285dc31200d
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2024-06-22 13:08:37 +0530

    update project

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -6,13 +6,6 @@ import AdminAddProduct from './page/admin/admin-add-product.page.tsx';
 import CusotmerLogin from './page/cusotmer/customer-login.page.tsx';
 import CustomerRegister from './page/cusotmer/customer-regiseter.page.tsx';
 import CustomerHome from './page/cusotmer/customer-home.page.tsx';
-import { WomensCasualWear } from './page/cusotmer/womens-wear/casual-wear.page.tsx';
-import { WomensFormalWear } from './page/cusotmer/womens-wear/formal-wear.page.tsx';
-import { WomensEthnicWear } from './page/cusotmer/womens-wear/ethnic-wear.page.tsx';
-import { WomensLingerieInnerWear } from './page/cusotmer/womens-wear/lingerie-innerwear.page.tsx';
-import { WomensNightWear } from './page/cusotmer/womens-wear/night-wear.page.tsx';
-import { WomenSportsWear } from './page/cusotmer/womens-wear/sports-wear.page.tsx';
-import WomenAccessories from './page/cusotmer/womens-wear/accessories.page.tsx';
 import ProductView from './page/cusotmer/product-view.page.tsx';
 import ProductList from './page/cusotmer/product-list.page.tsx';
 
@@ -33,13 +26,6 @@ function App() {
         <Route path='/' element={<CustomerHome />}/>
           <Route path='login' element={<CusotmerLogin />}/>
           <Route path='register' element={<CustomerRegister />}/>
-          <Route path='/categories/women/casual-wear' element={<WomensCasualWear />}/>
-          <Route path='/categories/women/formal-wear' element={<WomensFormalWear />}/>
-          <Route path='/categories/women/ethnic-wear' element={<WomensEthnicWear />}/>
-          <Route path='/categories/women/lingerie-wear' element={<WomensLingerieInnerWear />}/>
-          <Route path='/categories/women/nightwear-wear' element={<WomensNightWear />}/>
-          <Route path='/categories/women/sports-wear' element={<WomenSportsWear />}/>
-          <Route path='/categories/women/accessories' element={<WomenAccessories />}/>
           <Route path='/categories/top/product/featured/:productId' element={<ProductView />}/>
           <Route path='/category/:categoryName/:subCategory' element={<ProductList />}/>
         </Routes>
--- FRONTEND/src/page/cusotmer/customer-home.page.tsx
+++ FRONTEND/src/page/cusotmer/customer-home.page.tsx
@@ -3,6 +3,8 @@
 import { useId, useState } from "react";
 import { DemoLayout } from "../../layout/demo.layout";
 import ProductCard from "../../component/product-card-2.component";
+import { Link } from "react-router-dom";
+import { footWearNavList, kidsNavList, menNavList, womenNavList } from "../../assets/navigation.list";
 
 const categoreyImages: string[] = [
     "https://www.ecomdeveloper.com/demo/image/cache/catalog/category/1-180x180.png",
@@ -32,10 +34,10 @@ export default function CustomerHome() {
                 <DemoLayout>
                     <section className="flex justify-between mt-5">
                         {/* category listed card */}
-                        <CategoreyCard mainCategory="Women" image={categoreyImages[0]} />
-                        <CategoreyCard mainCategory="Men" image={categoreyImages[1]} />
-                        <CategoreyCard mainCategory="Kids" image={categoreyImages[2]} />
-                        <CategoreyCard mainCategory="Footwear" image={categoreyImages[3]} />
+                        <CategoreyCard subCategory={womenNavList} mainCategory="Women" image={categoreyImages[0]} />
+                        <CategoreyCard subCategory={menNavList} mainCategory="Men" image={categoreyImages[1]} />
+                        <CategoreyCard subCategory={kidsNavList} mainCategory="Kids" image={categoreyImages[2]} />
+                        <CategoreyCard subCategory={footWearNavList} mainCategory="Footwear" image={categoreyImages[3]} />
                     </section>
 
                     <section className="">
@@ -109,19 +111,27 @@ function LatestProduct(){
 interface CategoryProp {
     mainCategory: string
     image: string
+    subCategory:any[]
 }
 function CategoreyCard(prop: CategoryProp) {
     return (
         <>
-            <div className="bg-[#9c4399] bg-opacity-10 p-3 rounded-xl flex gap-2">
-                <div>
+            <div className="bg-[#9c4399] bg-opacity-10 p-3 rounded-xl flex gap-2 ">
+                <div className="flex-auto">
                     <p className="font-bold text-xl">{prop.mainCategory}</p>
                     <ul className="pl-3 mt-2">
-                        <li>Jewelry</li>
+                        {prop.subCategory.map((element) => {
+                            return (
+                                <li className="text-nowrap">
+                                    <Link to={element.url} reloadDocument>{element.name}</Link>
+                                </li>
+                            )
+                        })}
+                        {/* <li>Jewelry</li>
                         <li>Fashions</li>
                         <li>Accessories</li>
                         <li>Jewelery</li>
-                        <li>Fashions</li>
+                        <li>Fashions</li> */}
                     </ul>
                 </div>
                 <div className="w-full">
