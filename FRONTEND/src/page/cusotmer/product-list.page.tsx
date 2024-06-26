55b85abc1460dcd8aff22e6d48a44d458102121e
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2024-06-26 12:02:57 +0530

    fetch product

--- FRONTEND/src/page/cusotmer/product-list.page.tsx
+++ FRONTEND/src/page/cusotmer/product-list.page.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import ProductViewLayout from "../../layout/product-view.layout";
+import axios from "axios";
+import { useParams } from "react-router";
+import ProductCard from "../../component/product-card.component";
 
-export default function ProductList(){
+
+export default function ProductList() {
+
+    const { categoryName, subCategory } = useParams();
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetchProducts()
+    });
+
+    async function fetchProducts() {
+        try {
+            const response = await axios.get(`http://localhost:5000/api/v1/product/${categoryName}/${subCategory}`);
+            setProducts(response.data.data)
+        } catch (errr) {
+            console.error(errr);
+        }
+    }
+
     return (
         <>
             <ProductViewLayout>
-            
+                <div className="w-full mt-5">
+                    {products.map((element) => <ProductCard />)}
+                </div>
             </ProductViewLayout>
         </>
 
