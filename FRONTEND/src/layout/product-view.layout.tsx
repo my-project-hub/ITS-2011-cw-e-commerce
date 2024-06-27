3c0b6a101d41e8fe5da17a012af9ceb31fa7cf91
Dilshan Shivantha <shiwanthadilshan3@gmail.com>
2024-06-27 07:02:13 +0530

    create cart component and context

--- FRONTEND/src/layout/product-view.layout.tsx
+++ FRONTEND/src/layout/product-view.layout.tsx
@@ -4,6 +4,7 @@ import { faCcMastercard, faCcVisa, faFacebookF, faInstagram, faPinterest, faTwit
 import { footWearNavList, kidsNavList, menNavList, womenNavList } from "../assets/navigation.list";
 import { authorization } from "../util/authorization";
 import { useNavigate } from "react-router";
+import CartComponent from "../component/cart.component";
 
 export default function ProductViewLayout({ children }: React.PropsWithChildren) {
 
@@ -25,7 +26,7 @@ export default function ProductViewLayout({ children }: React.PropsWithChildren)
                                 <NavbarOptions category='KIDS' subCategory={kidsNavList} />
                                 <NavbarOptions category='FOOT WEAR' subCategory={footWearNavList} />
                             </div>
-                            <div className="text-sm flex items-center gap-3">
+                            <div className="text-sm flex items-center gap-3 relative">
                                 {
                                     authorization() ?
                                         <div onClick={() => {
@@ -46,9 +47,12 @@ export default function ProductViewLayout({ children }: React.PropsWithChildren)
                                 <div>
                                     <svg data-v-4d526081="" xmlns="http://www.w3.org/2000/svg" className="h-3.5 lg:h-5 z-10 text-black" viewBox="0 0 24 24"><g data-v-4d526081="" fill="none"><path data-v-4d526081="" d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"></path><path data-v-4d526081="" fill="currentColor" d="M18.06 3a2 2 0 0 1 1.98 1.719l.017.156l.875 14a2 2 0 0 1-1.847 2.12l-.15.005H5.066a2 2 0 0 1-2-1.976l.003-.149l.875-14a2 2 0 0 1 1.84-1.869L5.94 3zm0 2H5.94l-.875 14h13.87zM15 7a1 1 0 0 1 1 1a4 4 0 0 1-8 0a1 1 0 0 1 2 0a2 2 0 0 0 3.995.15L14 8a1 1 0 0 1 1-1"></path></g></svg>
                                 </div> */}
-                                <div>
+                                {/* <div>
                                     <svg data-v-4d526081="" xmlns="http://www.w3.org/2000/svg" className="h-3.5 lg:h-5 z-10 text-black" viewBox="0 0 24 24"><g data-v-4d526081="" fill="none"><path data-v-4d526081="" d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"></path><path data-v-4d526081="" fill="currentColor" d="M18.06 3a2 2 0 0 1 1.98 1.719l.017.156l.875 14a2 2 0 0 1-1.847 2.12l-.15.005H5.066a2 2 0 0 1-2-1.976l.003-.149l.875-14a2 2 0 0 1 1.84-1.869L5.94 3zm0 2H5.94l-.875 14h13.87zM15 7a1 1 0 0 1 1 1a4 4 0 0 1-8 0a1 1 0 0 1 2 0a2 2 0 0 0 3.995.15L14 8a1 1 0 0 1 1-1"></path></g></svg>
-                                </div> 
+                                </div>  */}
+                                <div>
+                                <CartComponent />
+                                </div>
                             </div>
 
                         </div>
@@ -183,6 +187,7 @@ export default function ProductViewLayout({ children }: React.PropsWithChildren)
                     <div className='flex gap-2 justify-center'>
                         <FontAwesomeIcon icon={faCcVisa} className='text-2xl' />
                         <FontAwesomeIcon icon={faCcMastercard} className='text-2xl' />
+                        {/* <CartComponent /> */}
                     </div>
                 </div>
             </footer>
